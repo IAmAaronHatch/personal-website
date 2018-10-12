f2724c6029969a7d5f77cba0fc4023bb1821ddb1
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2018-10-12 14:32:52 -0700

    url

--- src/components/Kahoot.js
+++ src/components/Kahoot.js
@@ -17,7 +17,7 @@ export default class  extends Component {
                     <p className='text about' id='project-info'>Currently working on a Silver Platter. Users will be able to log in, find their location, and search for five randomized local eateries near them. I wanted to challenge myself to learn and work with mapping through esri.<br /><br />
                     <a style={styles}href='https://github.com/IAmAaronHatch/foodServiceGP'>Github Repo</a>
                 <br/>
-                <br/><a href='https://silverplatter.iamaaronhatch.com' style={styles}>Silver Platter Live Site</a></p>
+                <br/><a href='https://silverplatter.aaronharrisinfo.com' style={styles}>Silver Platter Live Site</a></p>
                    <img className='project-preview' src={SilverP1} alt='home page' />
                    <p className='text about' >Technologies used: React, Redux, Node, Express, Esri, Massive, Auth0, Jest</p>
                    <Link to='/' className='text'>Home</Link>
